6fab50281022e4fca378db54298a7146662af137
Vishal Raj <72160405+vishal11221@users.noreply.github.com>
2023-07-17 17:11:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ audiogo = new Audio('gameover.mp3');
 setTimeout(() => {
     audio.play()
 }, 1000);
+
 document.onkeydown = function (e) {
     console.log("Key code is: ", e.keyCode)
     if (e.keyCode == 38) {
@@ -15,11 +16,13 @@ document.onkeydown = function (e) {
             dino.classList.remove('animateDino')
         }, 700);
     }
+    
     if (e.keyCode == 39) {
         dino = document.querySelector('.dino');
         dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
         dino.style.left = dinoX + 112 + "px";
     }
+    
     if (e.keyCode == 37) {
         dino = document.querySelector('.dino');
         dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
